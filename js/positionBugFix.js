0c4d5805eb9a414a58e84670e2253918b0b58b12
duyiqi17 <duyiqi17@outlook.com>
2015-08-04 20:05:04 +0800

    v3.0
    完成大部分布局，差动画及兼容性适配

---
+++ js/positionBugFix.js
@@ -0,0 +1,42 @@
+/*用于修复翻页按钮的居中对齐问题*/
+var page=$("#pageChange >ul");
+var pageLiNumber=page.children().length;
+page.width(pageLiNumber*40);
+
+
+var lists=$(".list").children('li');
+var hotNews=$("#hotNews >ul").children('li');
+fixPositon(lists,"h1","p");
+fixPositon(hotNews,"p");
+fixHeight(hotNews); 
+/*用于修复因绝对定位引起的无图片标题的位置问题*/
+function fixPositon(elem,tag1,tag2,tag3){
+	 $.each(elem,function(){
+ 	var img=$(this).children('a').children('img').length;
+ 	if(img===0){
+ 		var tag1=$(this).children('a').children(tag1);
+ 		var tag2=$(this).children(tag2);
+ 		tag1.css("left","10px");
+ 		tag1.css({
+ 			width: '95%',
+ 			marginLeft: '0px',
+ 			marginTop:'0px'
+ 		});
+ 		tag2.css("left","10px");
+ 		}
+ 	})
+}
+/*用于修复右侧新闻栏因绝对定位引起的布局问题*/
+function fixHeight(elem){
+	 $.each(elem,function(){
+ 		var top=$(this).children('a').children('em').css('top');
+ 		if(top){
+ 			var emHeight=$(this).children('a').children('em').css('height');
+ 			var sumHeight=parseInt(emHeight+top);
+ 			if(sumHeight>60){
+ 				$(this).height(sumHeight+20);
+ 			}
+ 		}
+ 		
+ 	})
+}
