0dd86c5b9de02d0f11e2dcd27d7a2f5a715e64cf
duyiqi17 <duyiqi17@outlook.com>
2015-08-07 00:36:37 +0800

    v3.5
    内设机构完成，差首页动画，全局回到顶部和与数据库之间的交互

--- js/department.js
+++ js/department.js
@@ -35,3 +35,43 @@ function changeColor(){
 	$(this).find('a').css('color', '#0c66a9');
 });
 }
+//实现部门领导图片点击的效果
+var leaderInfo= new Array(
+	new Array("于乐","学生活动中心209","61830782","yule@uestc.edu.cn","学工部办公室、学生教育科、网络文化建设办公室"),
+	new Array("吴绪红","学生活动中心207A","61830081","poffice@uestc.edu.cn","资助中心、沙河管理办公室、武装部"),
+	new Array("刘惠","学生活动中心304","61830125","liuhui@uestc.edu.cn","文化艺术教育中心 国家大学生文化素质教育基地"),
+	new Array("李媛","学生活动中心405","61830396","liyuan@uestc.edu.cn","心理健康中心"),
+	new Array("张军","学生活动中心106A","61830130","2585766@qq.com","校团委"),
+	new Array("杜辉","学生活动中心209","61830782","duhui@uestc.edu.cn","学生就业指导中心、学生管理科")
+	);
+var picsLi=$("#leaders > ul").children('li');
+picsLi.each(function() {
+	var imgs=$(this).find('img');
+	var number=imgs.attr("alt");
+	var picAdd=imgs.attr("src");
+	imgs.click(function() {
+		var picSrc=picAdd;
+		var name=leaderInfo[number][0];
+		var place="办公地点："+leaderInfo[number][1];
+		var phone="办公电话："+leaderInfo[number][2];
+		var mail="工作邮箱："+leaderInfo[number][3];
+		var depart="分管部门："+leaderInfo[number][4];
+		var p="<p>"+place+"</br>"+phone+"</br>"+mail+"</br>"+depart+"<p</p>";
+		var img="<img src=\""+picSrc+"\">";
+		var h1="<h1>"+name+"</h1>";
+		$('#main').append('<div class=\"cover\"><div class=\"leaderInfo\">'+img+h1+p+'</div></div>');
+		positonFix();
+		var cover=$('.cover');
+		cover.click(function() {
+			event.stopPropagation();
+			cover.remove();
+		});
+
+	});
+	
+});
+// //解决部门领导图片点击后的排版问题
+function positonFix(){
+	var newHeight=$(window).height()-$(".leaderInfo").height();
+	$(".leaderInfo").css('marginTop', function(){return newHeight/2});
+}
